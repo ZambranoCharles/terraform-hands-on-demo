e881592443858f78b5a594d9781213c8b375a36d
Charles Zambrano <charles.zambrano@northhighland.com>
2026-02-05 11:03:10 -0500

    chore(terraform): add input variables for suffix and owner

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "suffix" {
+  type        = string
+  description = "Short suffix for resource names"
+  default     = "001"
+}
+
+variable "owner" {
+  type        = string
+  description = "Name of the workshop owner"
+  default     = "student"
+}
